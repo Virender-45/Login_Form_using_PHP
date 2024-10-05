e606116c825e0e8dcdad282a22e089a908c954fc
Virender Rana <143004956+Virender-45@users.noreply.github.com>
2024-10-05 10:45:55 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,6 +2,14 @@
 require 'includes/header.php';
 //session_destroy(); //logout from the current session , also if the page refreshes
 
+if (isset($_SESSION['username'])) {
+    $username = $_SESSION['username'];
+    echo "<h1>Hello, $username</h1>";
+} else {
+    echo "<h1>Hello, guest!</h1>";
+}
+
+
 ?>
 
 <h1>Hello user!</h1>
